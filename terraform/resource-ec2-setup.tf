3bf06db0bee0a39b5bded99a24665b1404f73d24
Pavel <serfer94@gmail.com>
2024-12-02 18:33:06 +0100

    correct values for Grafana with new prometheus setup

--- terraform/resource-ec2-setup.tf
+++ terraform/resource-ec2-setup.tf
@@ -250,7 +250,7 @@ resource "aws_spot_instance_request" "k3s_control_plane" {
               helm repo update
               #git clone -b task_7 https://github.com/Proffesor94/rsschool-devops-course-tasks.git /opt/conf/task_7
               git clone -b task_8 https://github.com/Proffesor94/rsschool-devops-course-tasks.git /opt/conf/task_8
-              helm upgrade --install prometheus bitnami/kube-prometheus --namespace monitoring --create-namespace -f /opt/conf/task_7/helm/prometheus/values.yaml
+              helm upgrade --install prometheus bitnami/kube-prometheus --namespace monitoring --create-namespace -f /opt/conf/task_8/helm/prometheus/values.yaml
               helm upgrade --install grafana bitnami/grafana --namespace monitoring --create-namespace -f /opt/conf/task_8/helm/grafana/values.yaml
               EOF
 }
